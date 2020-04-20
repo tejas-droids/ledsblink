4738395cb3e4323872c087d4426aa2369847a6a9
tejas-droids <54080749+tejas-droids@users.noreply.github.com>
2020-04-20 10:12:55 +0530

    Update ledsblink.h

--- ledsblink/ledsblink.h
+++ ledsblink/ledsblink.h
@@ -4,31 +4,27 @@
 */
 
 // ensure this library description is only included once
-#ifndef segment_h
-#define segment_h
+#ifndef ledsblink_h
+#define ledsblink_h
 
 // include types & constants of Wiring core API
 #include "Arduino.h"
 
 // library interface description
-class segment
+class ledsblink
 {
   // user-accessible "public" interface
   public:
-   fourdigital(int PinFirst, int PinSecond, int PinThird, int PinFourth);
-   eightsegmental(int segA, int segB, int segC, int segD, int segE, int segF,int segG);
+   ledsblink(int PinFirst, int PinSecond, int PinThird, int PinFourth, int PinFifth);
     
-	void  lightnumber(int number to display);
-
-	void on(int pin_number);
+	void flash(int delayTimeForFlash);
+	void on();
 	void off();
 
   // library-accessible "private" interface
   private:
-    int _PinFirst, _PinSecond, _PinThird, _PinFourth;
-    int segA, int segB, int segC, int segD, int segE, int segF,int segG;
-	
-	void  lightnumber(int number_to_display);
+    int _PinFirst, _PinSecond, _PinThird, _PinFourth, _PinFifth;
+    
 };
 
 #endif
